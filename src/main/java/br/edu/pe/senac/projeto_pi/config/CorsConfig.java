10fff5bf7cb80e7a820f62bc6f19634b65747424
Abigail Nazário <136490258+AbigailNazario@users.noreply.github.com>
2026-04-13 22:28:02 -0300

    Liberando o netlify

--- src/main/java/br/edu/pe/senac/projeto_pi/config/CorsConfig.java
+++ src/main/java/br/edu/pe/senac/projeto_pi/config/CorsConfig.java
@@ -21,7 +21,8 @@ public class CorsConfig {
             "http://localhost:8080",
             "http://127.0.0.1:8080",
             "http://localhost:5173",
-            "http://127.0.0.1:5173"
+            "http://127.0.0.1:5173",
+            "https://seu-site.netlify.app"
         ));
         
         // Permite todos os métodos HTTP
