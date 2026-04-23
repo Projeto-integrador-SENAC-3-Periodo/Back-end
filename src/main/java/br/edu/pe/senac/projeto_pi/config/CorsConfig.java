2f0ec9269a670e729eb1af944ce83915991d27e0
Abigail Nazário <136490258+AbigailNazario@users.noreply.github.com>
2026-04-22 22:50:21 -0300

    Update allowed origins in CorsConfig para teste local

--- src/main/java/br/edu/pe/senac/projeto_pi/config/CorsConfig.java
+++ src/main/java/br/edu/pe/senac/projeto_pi/config/CorsConfig.java
@@ -20,8 +20,8 @@ public class CorsConfig {
         configuration.setAllowedOrigins(Arrays.asList(
             "http://localhost:8080",
             "http://127.0.0.1:8080",
-            "http://localhost:5173",
-            "http://127.0.0.1:5173",
+            "http://localhost:5174",
+            "http://127.0.0.1:5174",
             "https://horascomplementaressenac.netlify.app"
         ));
         
